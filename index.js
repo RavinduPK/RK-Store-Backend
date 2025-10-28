0413cbf2ab207e988147c71293957b9542124db3
Rama <ravindukalhara215@gmail.com>
2025-10-29 01:41:27 +0530

    address update

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.post(
     }
     const handleLogin = async (e) => {
   e.preventDefault();
-  const res = await fetch("http://localhost:5000/login", {
+  const res = await fetch("http://localhost:27017/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
